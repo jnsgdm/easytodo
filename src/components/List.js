1c113fca8dca38b2a533564f3b12ec0a78f269a1
Jonas G D'angelo <JonasDmoreira@hotmail.com>
2022-11-20 11:37:18 -0300

    feat: add task and check task done

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,35 @@
-import {useState,useReducer} from 'react'
+import {useState} from 'react'
 
+import './List.css';
 import { BiMessageSquareAdd } from 'react-icons/bi'
 
 const List = () => {
   const [valueTask,setValueTask] = useState("");
   const [taskList,setTaskList] = useState([]);
 
+  const handleTaskAdd = (event) => {
+    event.preventDefault();
+    const newTask = {
+      id: taskList.length + 1,
+      value: valueTask,
+      haveDone: false
+    }
+    setTaskList([...taskList, newTask]);
+  }
+
+  const handleTaskDone = (task) => {
+    const updatedTaskList = taskList.map((item) => {
+      if (item.id === task.id) {
+        item.haveDone = !task.haveDone;
+      }
+      return item;
+    });
+    setTaskList(updatedTaskList);
+  }
+
   return (
     <div className='list-container'>
-      <form onSubmit={handleSubmitTask}>
+      <form onSubmit={handleTaskAdd}>
         <input className='input-task' type="text" value={valueTask} onChange={(event) => setValueTask(event.target.value)} placeholder='Para adicionar uma tarefa basta digita-la e apertar enter!' />
         <button type='subimt' className='send-btn'><BiMessageSquareAdd className='send-icon'/></button>
       </form>
@@ -16,7 +37,7 @@ const List = () => {
         <ul>
           {taskList.map((task) => (
             <div key={task.id}>
-              <li>
+              <li onClick={() => handleTaskDone(task)} className={task.haveDone ? 'done' : 'not-done'}>
                 {task.id}. {task.value}
               </li>
             </div>
